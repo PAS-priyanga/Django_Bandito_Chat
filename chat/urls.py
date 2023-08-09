64586441f0588eaa2d65243c95997c275c00eebc
priyanga_pas <apriyangasingh@gmail.com>
2023-08-09 00:27:46 -0700

    Added urls

--- chat/urls.py
+++ chat/urls.py
@@ -1,17 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
-from chat import views as chat_views
+from django.urls import path
+from chat import views
 
-
-#from django.contrib.auth.views import LoginView, LogoutView
- 
- 
+# from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
-    path("", chat_views.home, name="home"),
-    path("chat/", chat_views.chatRoom, name="chat"),
-    path('messages/', chat_views.messages, name='message'),
-    # path('messages/<int:messages_id>/', chat_views.messages_detail, name='detail')
-
- 
-    # login-section
-  ]
+    path("", views.home, name="home"),
+    path('about/', views.about, name='about'),
+    path("chat/", views.chatRoom, name="chatRoom"),
+    path("messages/", views.messages, name="message"),
+    path('messages/<int:messages_id>/', views.message_detail, name='messageDetail'),
+    path('messages/create/', views.MessageCreate.as_view(), name='message_create'),
+    path('messages/<int:pk>/update/', views.MessageUpdate.as_view(), name='messages_update'),
+    path('messages/<int:pk>/delete/', views.MessageDelete.as_view(), name='messages_delete')
+]
--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render
+from .models import *
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
   return render(request, 'chat/base.html')
 
-def chatPage(request):
-    return render(request, "chat/chatPage.html")
+def about(request):
+  return render(request, 'chat/about.html')
 
 def chatRoom(request):
    return render(request, "chat/chatRoom.html")
@@ -13,3 +16,20 @@ def chatRoom(request):
 def messages(request):
    return render(request, "chat/messages.html")
 
+def message_detail(request, message_id):
+   message = Message.objects.get(id=message_id)
+   return render(request, 'message/detail.html', {
+    'message': message
+  })
+
+class MessageCreate(LoginRequiredMixin,CreateView):
+  model = Message
+  fields = ['Content', 'SentAt']
+
+class MessageUpdate(LoginRequiredMixin,UpdateView):
+  model = Message
+  fields = ['Content', 'SentAt']
+
+class MessageDelete(LoginRequiredMixin,DeleteView):
+  model = Message
+  success_url = '/messageDelete'
